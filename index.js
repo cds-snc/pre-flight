ae34f4560126122af2f51e3837dff38d320877cb
Tim Arney <tim@line37.com>
2019-03-12 05:56:32 -0400

    update handle event

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 require = require("esm")(module); // eslint-disable-line no-global-assign
-require("dotenv-safe").config({ allowEmptyValues: true });
+// require("dotenv-safe").config({ allowEmptyValues: true });
 const { handler } = require("./src/lib/handler");
-module.exports.handleEvent = handler;
+
+const handleEvent = async (request, response) => {
+  await handler(request);
+  response.status(200).send("Done!");
+};
+
+module.exports.handleEvent = handleEvent;
