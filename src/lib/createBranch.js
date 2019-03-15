ba1fd24e22b2d0f806155b2b664459f3923944de
Max Neuvians <max@neuvians.net>
2019-03-15 13:52:44 -0400

    Read files and create paths (#7)

--- src/lib/createBranch.js
+++ src/lib/createBranch.js
@@ -1,16 +1,20 @@
 import { authenticate } from "./githubAuth";
 
-export const createBranch = async (body, sha, name = "pre-flight") => {
+export const createBranch = async (
+  body,
+  sha,
+  name = `pre-flight-${Date.now()}`
+) => {
   const repoOwner = body.repository.owner.login;
   const repoName = body.repository.name;
   const ref = `refs/heads/${name}`;
   const client = await authenticate();
-  const result = await client.git.createRef({
+  await client.git.createRef({
     owner: repoOwner,
     repo: repoName,
     ref,
     sha
   });
 
-  return result;
+  return name;
 };
