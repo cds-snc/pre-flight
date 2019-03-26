c2e541a3d1af14d55f9eee231cb0cba88a09a4a7
Max <max@neuvians.net>
2019-03-26 13:27:32 -0400

    LICENSE rename

--- src/lib/createPr.js
+++ src/lib/createPr.js
@@ -6,7 +6,7 @@ export const createPr = async (body, head) => {
   const title = "CDS-SNC pre-flight checks";
   const base = "master";
   const prBody =
-    "Please consider merging the following files. If you would like to use our proposed CI/CD solution rename the `.github/pre-flight.workflow` to `.github/main.workflow`";
+    "Please consider merging the following files. If you would like to use our proposed CI/CD solution rename the `.github/pre-flight.workflow` to `.github/main.workflow`. Also consider using the LICENSE.suggested file for your LICENSE";
   const client = await authenticate();
 
   await client.pulls.create({
